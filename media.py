daad7602558df6f1fe9fed71f66517c782c9d1f6
Stuti Rastogi <stutirastogi92@gmail.com>
2018-08-23 23:26:43 +0530

    Added media model class

---
+++ media.py
@@ -0,0 +1,11 @@
+"""class to store movie details
+    title is the title of movie to be displayed
+    poster_image_url is the url of the thumbnail of movie
+    trailer_youtube_url is the link to youtube video of it's trailer"""
+
+
+class Movie:
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
