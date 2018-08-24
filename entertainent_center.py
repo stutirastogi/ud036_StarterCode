02e34cf3a7b374210fc53819fb2c99869b2e81e9
stutirastogi <stutirastogi92@gmail.com>
2018-08-24 08:41:05 +0530

    pep 8 guidelines

--- entertainent_center.py
+++ entertainent_center.py
@@ -1,10 +1,12 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "https://images-na.ssl-images-amazon.com/images/I/51Q3H0JX9FL._SY445_.jpg",
+toy_story = media.Movie("Toy Story",
+                        "https://images-na.ssl-images-amazon.com/images/I/51Q3H0JX9FL._SY445_.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 frozen = media.Movie("Frozen",
-                     "https://vignette.wikia.nocookie.net/world-of-media/images/1/16/Frozen.jpg/revision/latest?cb=20151202220439",
+                     "https://vignette.wikia.nocookie.net/world-of-media/images/1/16/Frozen.jpg/revision/latest?cb"
+                     "=20151202220439",
                      "https://www.youtube.com/watch?v=TbQm5doF_Uc")
 maleficent = media.Movie("Maleficent",
                          "https://geekxlovin.files.wordpress.com/2014/05/wpid-photo-11.jpg",
@@ -15,3 +17,4 @@ infinity_war = media.Movie("Infinity War",
 
 movies = [toy_story, frozen, maleficent, infinity_war]
 fresh_tomatoes.open_movies_page(movies)
+
